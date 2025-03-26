16443010e80ff2b8be4fde11aafe1d4294e509e9
Eric-BR2001 <eric.souza@hotmail.com>
2025-03-25 23:11:31 -0300

    Added the Sixth day.Using fuctions ROUND, AVG,MAX,MIN,CONCAT,COUNT AND CAST.

--- SQL/06/Day6.sql
+++ SQL/06/Day6.sql
@@ -1,5 +1,5 @@
 SELECT ad_size, ad_type, (available_impressions - impressions) AS difference FROM clean_ads_data LIMIT 50;
 SELECT ad_size, ad_type, MAX(available_impressions - impressions) AS max_difference FROM clean_ads_data GROUP BY ad_size, ad_type ORDER BY max_difference DESC LIMIT 1;
-select date_2,SUM(available_impressions) as available_impressions from clean_ads_data where date_2 IN("Friday","Thursday","Monday","Tuesday","Wednesday") group by date_2
+select date_2,SUM(available_impressions) as available_impressions from clean_ads_data where date_2 IN("Friday","Thursday","Monday","Tuesday","Wednesday") group by date_2;
 select date_2,(SUM(available_impressions)-SUM(impressions)) as available_impressions from clean_ads_data group by date_2;
-select date_2,(SUM(available_impressions)-SUM(impressions)) as available_impressions from clean_ads_data where hour_of_day BETWEEN 10 AND 17 group by date_2
+select date_2,(SUM(available_impressions)-SUM(impressions)) as available_impressions from clean_ads_data where hour_of_day BETWEEN 10 AND 17 group by date_2;
